7cfa922538a3a04c84198483625e88e64474e3ef
rednek21 <egor.podustov@mail.ru>
2024-07-02 13:09:06 +0300

    imstupid

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,7 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
+import "./outputTW.css";
+
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
--- services/alior-auth/main.go
+++ services/alior-auth/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
-	"alior-digital/src/database"
+	"alior-auth/src/database"
 
-	_ "alior-digital/migrations"
+	_ "alior-auth/migrations"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	var dbhost string
+	flag.StringVar(&dbhost,
+		"dbhost",
+		"localhost",
+		"")
+	flag.Parse()
 	ctx := context.Background()
-	pool, err := pgxpool.New(ctx, "postgres://auth:aliorAuth@localhost:5432/auth?sslmode=disable")
+	pool, err := pgxpool.New(ctx, "postgres://auth:aliorAuth@"+dbhost+":5432/auth?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
