883b4b3bce0e51a5c18573a6091f20659f197f66
rednek21 <egor.podustov@mail.ru>
2024-07-25 14:43:47 +0300

    update compose and req.txt

--- services/tg_bot/compile_proto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python -m grpc_tools.protoc -I./app/api/proto --python_out=./app/api/proto --grpc_python_out=./app/api/proto ./app/api/proto/tn.proto
-python -m grpc_tools.protoc -I./app/api/proto --python_out=./app/api/proto --grpc_python_out=./app/api/proto ./app/api/proto/cms.proto
-
-sed -i 's/import cms_pb2 as cms__pb2/from . import cms_pb2 as cms__pb2/' ./app/api/proto/cms_pb2_grpc.py
-
-echo "Proto compilation completed"
