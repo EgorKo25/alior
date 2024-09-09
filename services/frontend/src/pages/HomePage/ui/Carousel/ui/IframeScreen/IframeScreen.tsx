8f7fbe27077d93b4063224cb68a6b869eca72fbd
Leritas <din.fuso@mail.ru>
2024-09-09 21:28:17 +0300

    update size for 1024px

--- services/frontend/src/pages/HomePage/ui/Carousel/ui/IframeScreen/IframeScreen.tsx
+++ services/frontend/src/pages/HomePage/ui/Carousel/ui/IframeScreen/IframeScreen.tsx
@@ -22,7 +22,7 @@ export const IframeScreen: React.FC<{ url: string }> = ({ url }) => {
   }, [windowWidth]);
   return (
     <div className="screen-wrapper w-full h-full grid place-items-center max-w-[1020px]">
-      <div className="screen-parent relative w-full lg:w-[85%] xl:w-full aspect-[9/16] md:aspect-[4/5] xl:aspect-video">
+      <div className="screen-parent relative w-full lg:w-[85%] xl:w-full aspect-[9/16] md:aspect-[4/5] lg:aspect-[8.5/10] xl:aspect-video">
         <div className="screen-slice-1 z-[1]"></div>
         <div className="screen-slice-2 z-[2]"></div>
         <div className="screen-slice-3 z-[3]"></div>
@@ -55,7 +55,7 @@ export const IframeScreen: React.FC<{ url: string }> = ({ url }) => {
           >
             <iframe
               src={url}
-              className="iframe-content lg:rounded-40 border-solid md:border-8 border-transparent w-[450px] md:w-[1024px] xl:w-[1440px] aspect-[9/16] md:aspect-[4/5] xl:aspect-video absolute left-0 top-0"
+              className="iframe-content lg:rounded-40 border-solid md:border-8 border-transparent w-[450px] md:w-[1024px] xl:w-[1440px] aspect-[9/16] md:aspect-[4/5] lg:aspect-[8.5/10] xl:aspect-video absolute left-0 top-0"
               style={{
                 transform: `scale(${iframeScale})`,
                 transformOrigin: "top left",
