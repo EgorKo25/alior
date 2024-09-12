ccc364e902ed5b255d20b8add37e96f3ed0b027a
Leritas <din.fuso@mail.ru>
2024-09-12 13:30:28 +0300

    yappi logo animated

--- services/frontend/src/widgets/Header/Header.tsx
+++ services/frontend/src/widgets/Header/Header.tsx
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import clsx from "clsx";
@@ -25,12 +27,37 @@ export const Header = () => {
     };
   }, [isMenuOpen]);
 
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".header-logo", {
+      scale: 1,
+      left: "5%",
+      top: "28px",
+      delay: 1,
+      duration: 1.25,
+      transform: "translate(0,0)",
+    });
+
+    gsap.to(".white-bg-under-logo", {
+      opacity: 0,
+      backgroundColor: "transparent",
+      delay: 1,
+      duration: 1.5,
+    });
+  }, []);
+
   return (
     <header
       className={clsx(styles.header) + ` sm:justify-normal  lg:justify-between`}
     >
-      <div>
-        <LogoSVG className={""} />
+      <div className="white-bg-under-logo fixed top-0 left-0 w-screen h-screen bg-white pointer-events-none select-none z-40"></div>
+      <div className="w-[111px]">
+        <LogoSVG
+          className={
+            "header-logo absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-[3] md:scale-[4.5] lg:scale-[6] z-50"
+          }
+        />
       </div>
       <div className="hidden lg:flex">
         <Navbar isMobile={false} />
