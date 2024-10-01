57c001ed16bab33952c02dbef62a89125d3d243a
rednek21 <egor.podustov@mail.ru>
2024-10-01 15:27:54 +0300

    edit message via handler button call

--- services/alior-manager-bot/src/core/handlers.go
+++ services/alior-manager-bot/src/core/handlers.go
@@ -10,9 +10,9 @@ import (
 
 func (b *Bot) initHandlers() {
 	b.handlers["get_initial_callback"] = b.getInitialCallbackHandler
-	//b.handlers["previous_callback"] = b.getInitialCallbackHandler
+	b.handlers["previous_callback"] = b.getPreviousCallbackHandler
 	b.handlers["next_callback"] = b.getNextCallbackHandler
-	//b.handlers["delete_callback"] = b.getInitialCallbackHandler
+	b.handlers["delete_callback"] = b.deleteCallbackHandler
 }
 
 func (b *Bot) getInitialCallbackHandler(ctx context.Context, update *tgbotapi.Update) error {
@@ -70,15 +70,107 @@ func (b *Bot) getInitialCallbackHandler(ctx context.Context, update *tgbotapi.Up
 }
 
 func (b *Bot) getNextCallbackHandler(ctx context.Context, update *tgbotapi.Update) error {
-	b.logger.Info("in next msg handler")
-	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Следующее сообщение")
-	_, err := b.API.Send(msg)
+	err := broker.Publish("ask_publisher",
+		amqp091.Publishing{
+			ContentType: "text/plain",
+			Type:        "callback",
+			Headers:     amqp091.Table{"action_type": "next"},
+		},
+	)
 	if err != nil {
-		b.logger.Error("Ошибка при отправке сообщения: %v", err)
+		b.logger.Error("error getting next callback response", err)
 		return err
 	}
 
-	return err
+	response, err := broker.Consume("ans_consumer")
+	if err != nil {
+		b.logger.Error("error consuming next callback response", err)
+		return err
+	}
+
+	msg, ok := <-response
+	if !ok {
+		b.logger.Error("error getting next callback response")
+		return fmt.Errorf("error getting next callback response")
+	}
+	b.logger.Info("Получено сообщение: %s", msg.Body)
+
+	err = msg.Ack(false)
+	if err != nil {
+		b.logger.Error("Ошибка при подтверждении сообщения: %v", err)
+		return err
+	}
+
+	edit := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, string(msg.Body))
+
+	buttonPrev := tgbotapi.NewInlineKeyboardButtonData("Предыдущее", "previous_callback")
+	buttonNext := tgbotapi.NewInlineKeyboardButtonData("Следующее", "next_callback")
+	buttonDelete := tgbotapi.NewInlineKeyboardButtonData("Удалить", "delete_callback")
+
+	keyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(buttonPrev, buttonNext),
+		tgbotapi.NewInlineKeyboardRow(buttonDelete),
+	)
+
+	edit.ReplyMarkup = &keyboard
+
+	_, err = b.API.Send(edit)
+	if err != nil {
+		b.logger.Error("Ошибка при редактировании сообщения: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (b *Bot) getPreviousCallbackHandler(ctx context.Context, update *tgbotapi.Update) error {
+	b.logger.Info("in previous msg handler")
+
+	edit := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, "Предыдущее сообщение")
+
+	buttonPrev := tgbotapi.NewInlineKeyboardButtonData("Предыдущее", "previous_callback")
+	buttonNext := tgbotapi.NewInlineKeyboardButtonData("Следующее", "next_callback")
+	buttonDelete := tgbotapi.NewInlineKeyboardButtonData("Удалить", "delete_callback")
+
+	keyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(buttonPrev, buttonNext),
+		tgbotapi.NewInlineKeyboardRow(buttonDelete),
+	)
+
+	edit.ReplyMarkup = &keyboard
+
+	_, err := b.API.Send(edit)
+	if err != nil {
+		b.logger.Error("Ошибка при редактировании сообщения: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (b *Bot) deleteCallbackHandler(ctx context.Context, update *tgbotapi.Update) error {
+	b.logger.Info("in delete msg handler")
+
+	edit := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, "Сообщение удалено")
+
+	buttonPrev := tgbotapi.NewInlineKeyboardButtonData("Предыдущее", "previous_callback")
+	buttonNext := tgbotapi.NewInlineKeyboardButtonData("Следующее", "next_callback")
+	buttonDelete := tgbotapi.NewInlineKeyboardButtonData("Удалить", "delete_callback")
+
+	keyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(buttonPrev, buttonNext),
+		tgbotapi.NewInlineKeyboardRow(buttonDelete),
+	)
+
+	edit.ReplyMarkup = &keyboard
+
+	_, err := b.API.Send(edit)
+	if err != nil {
+		b.logger.Error("Ошибка при редактировании сообщения: %v", err)
+		return err
+	}
+
+	return nil
 }
 
 func (b *Bot) unknownCommandHandler(update *tgbotapi.Update) error {
--- services/alior-manager-bot/src/core/run.go
+++ services/alior-manager-bot/src/core/run.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"context"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func (b *Bot) Run(ctx context.Context) error {
@@ -12,20 +13,37 @@ func (b *Bot) Run(ctx context.Context) error {
 		updates := b.API.GetUpdatesChan(b.UpdateConfig)
 
 		for update := range updates {
-			if update.Message == nil || !update.Message.IsCommand() {
-				continue
+			if update.Message != nil && update.Message.IsCommand() {
+				b.logger.Info("Got command: %s", update.Message.Text)
+				command := update.Message.Command()
+
+				if handler, exists := b.handlers[command]; exists {
+					if err := handler(ctx, &update); err != nil {
+						b.logger.Error("handler error: %v", err)
+						errCh <- err
+					}
+				} else {
+					b.unknownCommandHandler(&update)
+				}
 			}
 
-			b.logger.Info("Got command: %s", update.Message.Text)
-			command := update.Message.Command()
+			if update.CallbackQuery != nil {
+				callbackData := update.CallbackQuery.Data
+				b.logger.Info("Got callback query: %s", callbackData)
 
-			if handler, exists := b.handlers[command]; exists {
-				if err := handler(ctx, &update); err != nil {
-					b.logger.Error("handler error: %v", err)
-					errCh <- err
+				if handler, exists := b.handlers[callbackData]; exists {
+					if err := handler(ctx, &update); err != nil {
+						b.logger.Error("callback handler error: %v", err)
+						errCh <- err
+					}
+				} else {
+					b.logger.Warn("Unknown callback data: %s", callbackData)
 				}
-			} else {
-				b.unknownCommandHandler(&update)
+
+				callback := tgbotapi.NewCallback(update.CallbackQuery.ID, "")
+				if _, err := b.API.Request(callback); err != nil {
+					b.logger.Error("Ошибка при ответе на CallbackQuery: %v", err)
+				}
 			}
 		}
 	}()
